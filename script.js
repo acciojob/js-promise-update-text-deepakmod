b2f938d458210e42b28da22cd25ee69683beafe6
Deepak Modanwal <118596712+deepakmod@users.noreply.github.com>
2023-04-27 16:46:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let div=document.getElementById("output");
+function promiseCreation(){
+	let promise=new Promise((resolve)=>{
+		setTimeout(()=>{resolve("Hello,world")},1000);
+	});
+	promise.then(updateFunction);
+	function updateFunction(str){
+		div.textContent=str;
+	}
+}
